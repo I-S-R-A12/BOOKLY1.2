9cf1c68f5ee518d8d2f89f7b95d823b32299fd98
Jhon Arl <naujralios@gmail.com>
2025-05-23 09:54:55 -0600

    personalizacion

--- lib/vistaPerfil/vistaPerfil.dart
+++ lib/vistaPerfil/vistaPerfil.dart
@@ -1,4 +1,4 @@
-//import 'package:firebase_database/firebase_database.dart';
+
 import 'package:bookly12/VentanaInicio/login.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';

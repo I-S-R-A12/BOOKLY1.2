34a47c6ac4ff39a38e30af0c30f99b908d155dd2
Leonsi-yo <urizardanilo8@gmail.com>
2025-05-23 14:33:18 -0600

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: publicar_libro(),
+          child: LibrosForm(),
         ),
       ),
     );
